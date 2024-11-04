bdfc51d869e74e096d068b8f5fa73fb40a0cd126
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2024-11-03 23:19:21 -0500

    Update and rename PP2_1.py to PP3_6.py

--- PP3_6.py
+++ PP3_6.py
@@ -8,9 +8,13 @@ def q2():
   #Write Assignment code here
 
 
+def q3(): 
+  #Write Assignment code here
+
 
 #Do not alter the following code
 #Comment out the following code when running your tests
 
 q1()
 q2()
+q3()
